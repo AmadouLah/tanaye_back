3f6e7f32e82d74552d6b0667e92e4dfad67f0a9c
Amadou Landoure <amadoulandoure004@gmail.com>
2025-09-21 22:08:27 +0000

    first commit

---
+++ src/main/java/com/tanaye/www/TanayeBackApplication.java
@@ -0,0 +1,13 @@
+package com.tanaye.www;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TanayeBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TanayeBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tanaye/www/TanayeBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tanaye.www;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TanayeBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
